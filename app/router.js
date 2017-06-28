d196e2a880e44cab2ae8793325547d87b1274e7d
Sivaneymar07 <sivayogesh72@gmail.com>
2017-06-28 18:31:50 +0530

    Add files via upload

---
+++ app/router.js
@@ -0,0 +1,57 @@
+app.config(['$routeProvider',function($routeProvider) {
+
+	$routeProvider
+		.when('/',{
+			templateUrl : 'partials/getStarted.html',
+			controller  : 'getStartedController',
+ 		})
+		.when('/login',{
+			templateUrl : 'partials/login.html',
+			controller  : 'loginController'
+		})
+		.when('/template',{
+			templateUrl : 'partials/template.html',
+		
+		})
+		.when('/template1',{
+			templateUrl : 'partials/template1.html',
+			controller  : 'template1Controller'
+		})
+		.when('/register',{
+			templateUrl : 'partials/register.html',
+			
+		})
+		.when('/websitetype',{
+			templateUrl : 'partials/websitetype.html',
+			
+		})
+		.when('/mobile',{
+			templateUrl : 'partials/mobile.html',
+		
+		})
+		.when('/home',{
+			templateUrl : 'partials/home.html',
+			
+		})
+		.when('/team',{
+			templateUrl : 'partials/team.html',
+			
+		})
+		.when('/portfolio',{
+			templateUrl : 'partials/portfolio.html',
+			
+		})
+		.when('/contactus',{
+			templateUrl : 'partials/contactus.html',
+			
+		})
+		.when('/dragdrop',{
+			templateUrl : 'partials/dragdrop.html',
+			controller  : 'dragController'
+			
+		})
+		.otherwise({
+			redirectTo : '/',
+			controller  : ''
+		})
+}]);
